c799c4cc9c0b716618707bee3b93ce844bd44dc9
Alexandra Ducati-Manas <alexandra.ducati-manas@st.com>
2025-03-11 11:34:31 +0100

    Improvement of debug logs for Card Emulation code
    Bug: 400752245
    Test: manual
    Flag: EXEMPT logging
    Change-Id: I7fe339149c2294259f68cb87b07449f6c9139e61

--- NfcNci/src/com/android/nfc/cardemulation/CardEmulationManager.java
+++ NfcNci/src/com/android/nfc/cardemulation/CardEmulationManager.java
@@ -81,7 +81,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -110,8 +109,9 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
         EnabledNfcFServices.Callback, WalletRoleObserver.Callback,
         PreferredSubscriptionService.Callback,
         HostEmulationManager.NfcAidRoutingListener {
-    static final String TAG = "CardEmulationManager";
+    static final String TAG = "NfcCardEmulationManager";
     static final boolean DBG = NfcProperties.debug_enabled().orElse(true);
+    static final boolean VDBG = NfcProperties.verbose_debug_enabled().orElse(true);
 
     static final int NFC_HCE_APDU = 0x01;
     static final int NFC_HCE_NFCF = 0x04;
@@ -379,6 +379,7 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
     }
 
     public void onUserSwitched(int userId) {
+        if (DBG) Log.d(TAG, "onUserSwitched");
         mWalletRoleObserver.onUserSwitched(userId);
         // for HCE
         mServiceCache.onUserSwitched();
@@ -402,6 +403,7 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
     }
 
     public void onNfcEnabled() {
+        if (DBG) Log.d(TAG, "onNfcEnabled");
         // for HCE
         mAidCache.onNfcEnabled();
         // for HCE-F
@@ -409,6 +411,7 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
     }
 
     public void onNfcDisabled() {
+        if (DBG) Log.d(TAG, "onNfcDisabled");
         // for HCE
         mAidCache.onNfcDisabled();
         // for HCE-F
@@ -636,6 +639,10 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
 
     boolean setDefaultServiceForCategoryChecked(int userId, ComponentName service,
             String category) {
+        if (DBG) {
+            Log.d(TAG, "setDefaultServiceForCategoryChecked: service=" + service + ", category="
+                    + category);
+        }
         if (!CardEmulation.CATEGORY_PAYMENT.equals(category)) {
             Log.e(TAG, "setDefaultServiceForCategoryChecked: Not allowing defaults for category "
                     + category);
@@ -790,6 +797,10 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
             if (!isServiceRegistered(userId, service)) {
                 return false;
             }
+            if (DBG) {
+                Log.d(TAG, "isDefaultServiceForCategory: service=" + service + ", category="
+                        + category);
+            }
             if (mWalletRoleObserver.isWalletRoleFeatureEnabled()) {
                 PackageAndUser holder =
                         mWalletRoleObserver.getDefaultWalletRoleHolder(userId);
@@ -831,6 +842,9 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
                 throws RemoteException {
             NfcPermissions.validateProfileId(mContext, userId);
             NfcPermissions.enforceAdminPermissions(mContext);
+            if (DBG) {
+                Log.d(TAG, "setDefaultForNextTap: service=" + service);
+            }
             if (service != null && !isServiceRegistered(userId, service)) {
                 return false;
             }
@@ -1196,9 +1210,12 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
                 return mWalletRoleObserver.getDefaultWalletRoleHolder(
                         callingUserId).getPackage() != null;
             }
-            String defaultComponent = Settings.Secure.getString(mContext.getContentResolver(),
-                    Constants.SETTINGS_SECURE_NFC_PAYMENT_DEFAULT_COMPONENT);
-            return defaultComponent != null ? true : false;
+            boolean isRegistered = Settings.Secure.getString(mContext.getContentResolver(),
+                    Constants.SETTINGS_SECURE_NFC_PAYMENT_DEFAULT_COMPONENT) != null;
+            if (DBG) {
+                Log.d(TAG, "isDefaultPaymentRegistered: " + isRegistered);
+            }
+            return isRegistered;
         }
 
         @Override
