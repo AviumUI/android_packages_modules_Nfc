6b8da938b4ac460358016d5e6b88ed20911f761a
Roshan Pius <rpius@google.com>
2025-04-21 08:26:33 -0700

    nfc: Fix overrideRoutingTable behavior
    1. In overrideRoutingTable, set the `defaultRoute` to match the `protocol`
    2. onUidToBackground to call RoutingOverridedOrRecovered
    
    Bug: 412272368
    Flag: EXEMPT bug fix
    (cherry picked from https://partner-android-review.googlesource.com/q/commit:8d741a8b3faccc50c4d9be68926256d4ba167fd6)
    Merged-In: Ica40b80267161f65e282cce6a482ef56af7d782d
    
    Change-Id: Ica40b80267161f65e282cce6a482ef56af7d782d

--- NfcNci/src/com/android/nfc/cardemulation/CardEmulationManager.java
+++ NfcNci/src/com/android/nfc/cardemulation/CardEmulationManager.java
@@ -1204,7 +1204,7 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
                         + ", technologyRoute " + technologyRoute);
             }
 
-//            mRoutingOptionManager.overrideDefaultRoute(protocolRoute);
+            mRoutingOptionManager.overrideDefaultRoute(protocolRoute);
             mRoutingOptionManager.overrideDefaultIsoDepRoute(protocolRoute);
             mRoutingOptionManager.overrideDefaultOffHostRoute(technologyRoute);
             int result = mAidCache.onRoutingOverridedOrRecovered();
@@ -1468,6 +1468,10 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
                     }
                     mForegroundUid = Process.INVALID_UID;
                     mRoutingOptionManager.recoverOverridedRoutingTable();
+                    if (mAidCache.onRoutingOverridedOrRecovered()
+                            != AidRoutingManager.CONFIGURE_ROUTING_SUCCESS) {
+                        Log.e(TAG, "recoverRoutingTable: onRoutingOverridedOrRecovered() failed");
+                    }
                 }
             }
         }
