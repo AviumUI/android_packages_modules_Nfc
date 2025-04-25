5db8377b7491fee239e00a92e0552d51a39b258d
Roshan Pius <rpius@google.com>
2025-04-25 12:57:00 -0700

    nfc: Propagate AID selected event to nfc event listeners.
    When aid is selected in SE, it will propagate to nfc event listeners to handle AID selection event.
    
    Also, renamed HostEmulationManager#onOffHostAidSelected to
    onOffHostAidSelectedOrTransaction since we are invoking that method for
    both events.
    
    Bug: 396186563
    Bug: 402346002
    Test: atest CtsNfcTestCases
    Flag: com.android.nfc.module.flags.event_listener_offhost_aid_selected
    Change-Id: I5952af95530bd4c4385f61e5ae12824d7ee50a6f

--- NfcNci/src/com/android/nfc/cardemulation/CardEmulationManager.java
+++ NfcNci/src/com/android/nfc/cardemulation/CardEmulationManager.java
@@ -361,8 +361,15 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
         }
     }
 
-    public void onOffHostAidSelected() {
-        mHostEmulationManager.onOffHostAidSelected();
+    public void onOffHostAidTransaction() {
+        mHostEmulationManager.onOffHostAidSelectedOrTransaction();
+    }
+
+    public void onOffHostAidSelected(@NonNull String aid, @NonNull String eeName) {
+        mHostEmulationManager.onOffHostAidSelectedOrTransaction();
+        if (android.nfc.Flags.nfcEventListener()) {
+            callNfcEventCallbacks(listener -> listener.onOffHostAidSelected(aid, eeName));
+        }
     }
 
     public void onBootCompleted() {
