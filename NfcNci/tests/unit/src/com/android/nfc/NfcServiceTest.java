1baf5e280aa9fcc9803ccd5e04236a297f843a20
Roshan Pius <rpius@google.com>
2025-04-30 17:03:47 -0700

    nfc: Ignore setSystemCodeRoute if NFC is being toggled off
    Bug: 414757863
    Test: Manual
    Flag: EXEMPT bug fix
    Change-Id: Iae40ff6e5a49f1333148bc74174ea4af61a0d14a

--- NfcNci/tests/unit/src/com/android/nfc/NfcServiceTest.java
+++ NfcNci/tests/unit/src/com/android/nfc/NfcServiceTest.java
@@ -1521,7 +1521,9 @@ public final class NfcServiceTest {
     }
 
     @Test
-    public void testSetSystemCodeRoute() {
+    public void testSetSystemCodeRoute() throws Exception {
+        enableAndVerify();
+
         mNfcService.setSystemCodeRoute(1);
         mLooper.dispatchAll();
         ArgumentCaptor<Integer> captor = ArgumentCaptor.forClass(Integer.class);
