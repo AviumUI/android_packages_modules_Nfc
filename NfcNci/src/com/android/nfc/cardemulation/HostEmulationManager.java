2387f985ccedaf983d2649ed0f4e0797f3f44ac6
Kyle Hsiao <kylehsiao@google.com>
2025-07-01 10:30:49 +0000

    nfc: Check if paymentServiceName is null when logging
    Bug: 428207248
    Test: manual
    Flag: EXEMPT bug fix
    Change-Id: I3a29aead046c7190b7dd4a24cd4bd53bcf6fa8ca

--- NfcNci/src/com/android/nfc/cardemulation/HostEmulationManager.java
+++ NfcNci/src/com/android/nfc/cardemulation/HostEmulationManager.java
@@ -1642,7 +1642,7 @@ public class HostEmulationManager {
                             new ComponentNameAndUser(mPaymentServiceUserId, name);
                     mComponentNameToConnectionsMap.remove(nameAndUser);
                 }
-                paymentServiceName = mPaymentServiceName;
+                if (mPaymentServiceName != null) paymentServiceName = mPaymentServiceName;
                 mPaymentService = null;
                 mPaymentServiceName = null;
             }
